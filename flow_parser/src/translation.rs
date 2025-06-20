919db52b9d27b31329e18d790f648c3ada47419d
Mohiul Haque <44129090+mohaque0@users.noreply.github.com>
2025-06-20 09:30:18 -0400

    Connect various components into flowi evaluation path.

--- flow_parser/src/translation.rs
+++ flow_parser/src/translation.rs
@@ -1,13 +1,27 @@
-use std::{collections::HashMap, sync::{atomic::AtomicUsize, Arc}};
+use std::collections::HashMap;
 
-use crate::{abt::{self, VarId}, ast};
+use crate::{abt::{self, Type, VarId}, ast, typecheck::{typecheck, TypecheckContext}};
 
-struct TranslationContext {
+#[derive(Clone)]
+pub struct TranslationContext {
+    typechecking_context: TypecheckContext,
     scoped_vars: HashMap<String, VarId>,
-    next_var_id: Arc<AtomicUsize>
+    operators: HashMap<OperatorType, VarId>,
 }
 
 impl TranslationContext {
+    pub fn new() -> Self {
+        return TranslationContext {
+            typechecking_context: TypecheckContext::new(),
+            scoped_vars: HashMap::new(),
+            operators: HashMap::new(),
+        };
+    }
+
+    fn get_typechecking_context(&self) -> &TypecheckContext {
+        return &self.typechecking_context;
+    }
+
     fn fresh_var(&mut self) -> VarId {
         return VarId::new();
     }
@@ -15,10 +29,33 @@ impl TranslationContext {
     fn set_var(&mut self, name: &String, id: VarId) -> Option<VarId> {
         self.scoped_vars.insert(name.clone(), id)
     }
+
+    fn get_var(&self, name: &String) -> Option<VarId> {
+        self.scoped_vars.get(name).cloned()
+    }
+
+    fn get_op(&self, op: &OperatorType) -> Option<VarId> {
+        self.operators.get(op).cloned()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+enum OperatorType {
+    Neg(Type),
+    Add(Type, Type),
+    Sub(Type, Type),
+    Mul(Type, Type),
+    Div(Type, Type),
+}
+
+#[derive(Clone, Debug)]
+pub enum TranslationError {
+    UndefinedOperation,
+    UnknownVariable(String),
 }
 
 
-fn translate(ctx: &mut TranslationContext, ast: &ast::Expr) -> abt::Expr {
+pub fn translate(ctx: &TranslationContext, ast: &ast::Expr) -> Result<abt::Expr, TranslationError> {
     match ast {
         ast::Expr::Value(value, debug_info) => {
             let value = match value {
@@ -27,18 +64,57 @@ fn translate(ctx: &mut TranslationContext, ast: &ast::Expr) -> abt::Expr {
                 ast::Value::String(v) => abt::Value::String(v.clone()),
             };
 
-            abt::Expr::Value(value, None)
+            Ok(abt::Expr::Value(value, None))
         },
-        ast::Expr::Var(v, debug_info) => todo!(),
+        ast::Expr::Var(v, debug_info) => {
+            ctx.get_var(v)
+                .map(|id| abt::Expr::Var(id))
+                .ok_or_else(|| TranslationError::UnknownVariable(v.clone()))
+        },
         ast::Expr::Neg(expr, debug_info) => todo!(),
-        ast::Expr::Add(expr, expr1, debug_info) => todo!(),
-        ast::Expr::Sub(expr, expr1, debug_info) => todo!(),
-        ast::Expr::Mul(expr, expr1, debug_info) => todo!(),
-        ast::Expr::Div(expr, expr1, debug_info) => todo!(),
+        ast::Expr::Add(e0, e1, debug_info) => {
+            let e0 = translate(ctx, e0)?;
+            let e1 = translate(ctx, e1)?;
+
+            let t0 = typecheck(ctx.get_typechecking_context(), &e0);
+            let t1 = typecheck(ctx.get_typechecking_context(), &e1);
+
+            if t0 == None || t1 == None {
+                return Err(TranslationError::UndefinedOperation);
+            }
+
+            let (t0, t1) = (t0.expect("Checked."), t1.expect("Checked."));
+            
+            if let Some(op) = ctx.get_op(&OperatorType::Add(t0, t1)) {
+                Ok(abt::Expr::Call { func: op, args: vec![e0, e1], debug: None })
+            } else {
+                Err(TranslationError::UndefinedOperation)
+            }
+        },
+        ast::Expr::Sub(e0, e1, debug_info) => todo!(),
+        ast::Expr::Mul(e0, e1, debug_info) => todo!(),
+        ast::Expr::Div(e0, e1, debug_info) => todo!(),
         ast::Expr::Function { args, body } => todo!(),
         ast::Expr::Enum { name, value } => todo!(),
         ast::Expr::Struct { name, fields } => todo!(),
-        ast::Expr::Let { name, rhs, then, debug } => todo!(),
+        ast::Expr::Let { name, rhs, then, debug } => {
+
+            let rhs = translate(&ctx, rhs)?;
+
+            let var_id = VarId::new();
+            let var_def = rhs;
+
+            let mut subctx: TranslationContext = ctx.clone();
+            subctx.set_var(name, var_id); // TODO: Must add to typechecking context as well.
+
+            let then = translate(&subctx, then)?;
+
+            Ok(abt::Expr::Let {
+                bind: HashMap::from([(var_id, var_def)]),
+                expr: Box::new(then),
+                debug: None
+            })
+        },
     }
 }
 
--- flowi/src/main.rs
+++ flowi/src/main.rs
@@ -1,5 +1,25 @@
+use chumsky::Parser as _;
+use clap::Parser as _;
+use flow_parser::{ast, translation};
+
 mod naive;
 
+#[derive(clap::Parser)]
+#[command(version, about, long_about = None)]
+struct CliOptions {
+    files: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = CliOptions::parse();
+
+    for file in &cli.files {
+        let filecontents = std::fs::read_to_string(file).expect(&format!("Could not read file: {}", file));
+        let parse_result = ast::parser().parse(&filecontents);
+        let ast = parse_result.output().expect("Failed to parse input");
+        let ctx = translation::TranslationContext::new();
+        let abt = translation::translate(&ctx, ast).expect("Failed to translate AST.");
+        let value = naive::eval(&abt);
+        println!("{:?}", value);
+    }
 }
